e8ccd6bc5539bceadfdf3440be2c1b52f7292038
Antonio Javier Alonso <anjalonrod@gmail.com>
2022-09-07 13:23:05 +0200

    logic: dice faces are now linked to a different variable, each one

--- script.js
+++ script.js
@@ -11,10 +11,23 @@ document.getElementById('total-0').textContent= '0';
 document.getElementById('total-1').textContent= '0'  //Every single variable and <div> concerning scores are set to zero('0' as string for <div> content).
 
 //DICE
+document.getElementById('dice-roll').addEventListener('click', function(){
+    dice = Math.floor(Math.random()*6)+ 1; //I created a 6 faces dice here using both methods 'random' and 'floor' on Math.
+    console.log('El dado tirado marca un' + ' ' + dice);
 
-dice = Math.floor(Math.random()*6)+ 1; //I created a 6 faces dice here using both methods 'random' and 'floor' on Math.
+    var img1 = document.getElementById('1.png');
+    var img2 = document.getElementById('2.png');
+    var img3 = document.getElementById('3.png');
+    var img4 = document.getElementById('4.png');
+    var img5 = document.getElementById('5.png');
+    var img6 = document.getElementById('6.png'); //These are the dice's faces in png which now belong to a variable respectively.
 
-console.log('El dado tirado marca un' + ' ' + dice);
+
+
+
+
+}
+)
 
 //
 
